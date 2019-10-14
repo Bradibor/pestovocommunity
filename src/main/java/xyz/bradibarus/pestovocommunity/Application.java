f0a01bb68d0946ffa7591e86f63f4c6681081537
bradibor <just1bomb@gmail.com>
2019-10-14 20:37:48 +0300

    heroku yml

--- src/main/java/xyz/bradibarus/pestovocommunity/Application.java
+++ src/main/java/xyz/bradibarus/pestovocommunity/Application.java
@@ -1,20 +1,31 @@
 package xyz.bradibarus.pestovocommunity;
 
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
-import xyz.bradibarus.pestovocommunity.model.Member;
 import xyz.bradibarus.pestovocommunity.repository.MemberRepository;
 
-import javax.annotation.PostConstruct;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
     @Autowired
     MemberRepository memberRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        OutputStream outputStream = new FileOutputStream("C:\\Users\\bradi\\IdeaProjects\\pestovocommunity\\src\\main\\resources\\kek.pdf");
+        PdfDocument pdfDocument = new PdfDocument();
+        PdfWriter pdfWriter = PdfWriter.getInstance(pdfDocument, outputStream);
+        PdfPTable pdfPTable = new PdfPTable(7);
     }
 }
